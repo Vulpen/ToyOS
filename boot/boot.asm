0fb63a8001f6738c404aa130f5afe43a9dee2655
Thomas Lowe <musicaltommy@gmail.com>
2023-03-17 13:51:46 -0400

    Lesson 2: Looping to clear the screen.

--- boot/boot.asm
+++ boot/boot.asm
@@ -3,15 +3,28 @@
 
 section code
 
-.main:
+.init:
     ; mov es, 0xb800 Ideally we do this to move this value to the es register
     ; ES can't be directly written to, as it's an 'intermediate' register
     ; This is the beginning of video memory!
     mov eax, 0xb800
     mov es, eax
+    mov eax, 0x00
 
+
+.clear: ; When QEMU starts, our BIOS prints a lotta junk to video memory, lets clear it cuz gross!!
+    mov byte [es:eax], 0
+    inc eax
+    mov byte [es:eax], 0x30
+    inc eax
+
+    cmp eax, 2 * 25 * 80
+    jl .clear
+
+.main:
     mov byte [es:0x00], 'H' ; move H to 0xb800 + 0x00
     mov byte [es:0x01], 0x30 ; color values
+
 
 jmp $
 
