851a6c2b97a1f8b88afe5d0797391415faa7c1c1
Thomas Lowe <musicaltommy@gmail.com>
2023-03-17 15:10:47 -0400

    Added print string functionality.

--- boot/boot.asm
+++ boot/boot.asm
@@ -4,13 +4,17 @@
 section code
 
 .init:
+    mov eax, 0x07c0    ; Prepare data segment
+    mov ds, eax 
     ; mov es, 0xb800 Ideally we do this to move this value to the es register
     ; ES can't be directly written to, as it's an 'intermediate' register
     ; This is the beginning of video memory!
     mov eax, 0xb800
     mov es, eax
-    mov eax, 0x00
-
+    mov eax, 0
+    mov ebx, 0  ; Index of the character in string
+    mov ecx, 0  ; Actual address of the character on screen
+    mov dl, 0   ; Store actual value of what we are printing
 
 .clear: ; When QEMU starts, our BIOS prints a lotta junk to video memory, lets clear it cuz gross!!
     mov byte [es:eax], 0
@@ -21,12 +25,31 @@ section code
     cmp eax, 2 * 25 * 80
     jl .clear
 
-.main:
-    mov byte [es:0x00], 'H' ; move H to 0xb800 + 0x00
-    mov byte [es:0x01], 0x30 ; color values
+    mov eax, text
+    push .end   ; Push the next location for 'ret' to return to
 
+.print:
+    mov dl, byte [eax + ebx]
 
-jmp $
+    cmp dl, 0
+    je .print_end
+
+    mov byte [es:ecx], dl
+    inc ebx
+    inc ecx
+    inc ecx
+
+    jmp .print
+
+.print_end:
+    mov eax, 0
+    ret
+
+.end:
+    jmp $
+
+text: db 'Hello, World!', 0
+text1: db 'This is another text.', 0
 
 times 510 - ($ - $$) db 0x00 ; Pads the file with 0s
 db 0x55
