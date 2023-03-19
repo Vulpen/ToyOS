a3dfd52777771e8edae24d384f60be342ec9e30a
Thomas Lowe <musicaltommy@gmail.com>
2023-03-19 12:49:44 -0400

    Compiling and loading in C code

--- boot/boot.asm
+++ boot/boot.asm
@@ -3,53 +3,61 @@
 
 section code
 
-.init:
-    ; mov es, 0xb800 Ideally we do this to move this value to the es register
-    ; ES can't be directly written to, as it's an 'intermediate' register
-    ; This is the beginning of video memory!
-    mov eax, 0xb800
-    mov es, eax
-    mov eax, 0
-    mov ebx, 0  ; Index of the character in string
-    mov ecx, 0  ; Actual address of the character on screen
-    mov dl, 0   ; Store actual value of what we are printing
+; .init:
+;     ; mov es, 0xb800 Ideally we do this to move this value to the es register
+;     ; ES can't be directly written to, as it's an 'intermediate' register
+;     ; This is the beginning of video memory!
+;     mov eax, 0xb800
+;     mov es, eax
+;     mov eax, 0
+;     mov ebx, 0  ; Index of the character in string
+;     mov ecx, 0  ; Actual address of the character on screen
+;     mov dl, 0   ; Store actual value of what we are printing
 
-.clear: ; When QEMU starts, our BIOS prints a lotta junk to video memory, lets clear it cuz gross!!
-    mov byte [es:eax], 0
-    inc eax
-    mov byte [es:eax], 0x02
-    inc eax
+; .clear: ; When QEMU starts, our BIOS prints a lotta junk to video memory, lets clear it cuz gross!!
+;     mov byte [es:eax], 0
+;     inc eax
+;     mov byte [es:eax], 0x02
+;     inc eax
 
-    cmp eax, 2 * 25 * 80
-    jl .clear
+;     cmp eax, 2 * 25 * 80
+;     jl .clear
 
-    mov eax, welcome
-    mov ecx, 0*2*80
-    call .print
-    jmp .switch
+;     mov eax, welcome
+;     mov ecx, 0*2*80
+;     call .print
+;     jmp .switch
 
-.end:
-    ;mov byte [es:0x00], 'L' ; Debug Character
-    jmp $
+; .end:
+;     ;mov byte [es:0x00], 'L' ; Debug Character
+;     jmp $
 
-.print:
-    mov dl, byte [eax + ebx]
+; .print:
+;     mov dl, byte [eax + ebx]
 
-    cmp dl, 0
-    je .print_end
+;     cmp dl, 0
+;     je .print_end
 
-    mov byte [es:ecx], dl
-    inc ebx
-    inc ecx
-    inc ecx
+;     mov byte [es:ecx], dl
+;     inc ebx
+;     inc ecx
+;     inc ecx
 
-    jmp .print
+;     jmp .print
 
-.print_end:
-    mov eax, 0
-    ret
+; .print_end:
+;     mov eax, 0
+;     ret
 
 .switch:
+    mov bx, 0x1000      ; Location of where the code is loaded from HDD
+    mov ah, 0x02
+    mov al, 30          ; Number of sectors to read
+    mov ch, 0x00
+    mov dh, 0x00
+    mov cl, 0x02
+    int 0x13
+
     cli     ; Disable interrupts
     lgdt [gdt_descriptor]   ; Load GDT Table
 
@@ -57,7 +65,7 @@ section code
     or eax, 0x1
     mov cr0, eax    ; Switch to protected mode
 
-    jmp protected_start
+    jmp code_seg:protected_start        ; Not sure why we need this offset
 
 welcome: db 'Welcome to PeepOS!', 0
 
@@ -74,6 +82,7 @@ protected_start:
     mov ebp, 0x90000
     mov esp, ebp
 
+    call 0x1000         ; Load into our C code!
     jmp $
 
 gdt_begin:
